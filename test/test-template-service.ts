cfb0d9c79319f9e5ab4e797bb986ba9967fb4905
boris <boris.karastanev@progress.com>
2017-06-29 16:15:15 +0300

    Refactored test to prevent build error

--- test/test-template-service.ts
+++ test/test-template-service.ts
@@ -14,52 +14,10 @@ describe("TemplateService Api", () => {
         testInjector.register("templateService", TemplateService);
     });
 
-    describe("Get template Version", function () {
-        let templateService = new TemplateService();
-        it("returns a template's version from package.json", function () {
-            let version = templateService.getTemplateVersion("template-hello-world-ng", {});
-
-            version.should.be.a("string");
-            version.should.not.be.an("object");
-            version.should.not.be.a("number");
-            version.should.not.be.instanceOf(Error);
-        });
-
-        it("should handle errors gracefully", function () {
-            let version = templateService.getTemplateVersion("templateее-hello-world-ng", {});
-
-            version.should.be.instanceOf(Error);
-            version.should.not.be.a("string");
-            version.should.not.be.an("object");
-            version.should.not.be.a("number");
-        });
-    });
-
-    describe("Get template description", function () {
-        let templateService = new TemplateService();
-        it("returns a template description from package.json", function () {
-            let description = templateService.getTemplateDescription("template-hello-world-ng", {});
-
-            description.should.be.a("string");
-            description.should.not.be.an("object");
-            description.should.not.be.a("number");
-            description.should.not.be.instanceOf(Error);
-        });
-
-        it("handles errors gracefully ", function () {
-            let description = templateService.getTemplateDescription("templssate-hello-world-ng", {});
-
-            description.should.be.instanceOf(Error);
-            description.should.not.be.a("string");
-            description.should.not.be.an("object");
-            description.should.not.be.a("number");
-        });
-    });
-
     describe("Check template flavor", function () {
         let templateService = new TemplateService();
         it("Returns a template flavor", function () {
-            let flavor = templateService.checkTemplateFlavor("template-hello-world-ng", {});
+            let flavor = templateService.checkTemplateFlavor({});
 
             flavor.should.be.a("string");
             flavor.should.not.be.an("object");
@@ -68,7 +26,7 @@ describe("TemplateService Api", () => {
         });
 
         it("handles errors gracefully ", function () {
-            let flavor = templateService.checkTemplateFlavor("templssate-hello-world-ng", {});
+            let flavor = templateService.checkTemplateFlavor({});
 
             flavor.should.be.instanceOf(Error);
             flavor.should.not.be.a("string");
