a4a79db3baedc2144ca1361fcfaca4bcc9e0d5ac
boris <boris.karastanev@progress.com>
2017-08-21 14:23:46 +0300

    Updated definitions file and removed all unused code in the template-service.ts file
    Signed-off-by: boris <boris.karastanev@progress.com>

--- lib/definitions/template-service.d.ts
+++ lib/definitions/template-service.d.ts
@@ -1,73 +1,14 @@
 interface ITemplateService {
-
     /**
-     * @description Get template's package.json from the master branch in the template's github repo
-     * @param templateName <String> - The name of the template
-     * @returns {Promise<any>} - Template's package.json file
-     */
-    tmpPackageJsonFromSrc(templateName: string): any;
-
-    /**
-     * @description Get template's resources from the github repo
-     * @param templateName <String>
-     * @param asset <Object> - asset name and platform
-     * @returns {Promise<any>} - Template's image recources in base64 encoding
-     */
-    tmpResourcesFromSrc(templateName: string, asset: any): any;
-
-    /**
-     * @description The method returns various meta data from template package.json
-     * @param packageJson <Object> - Valid package.json
-     * @returns {Promise<any>} - Template description, version, displayName, gitUrl
-     */
-    getTemplateMetaData(packageJson: any): any;
-
-    /**
-     * @description Check templates flavors E.g [@angularTs, vanillaJs, Ts ]
-     * @param packageJson <Object> - Valid package.json
-     * @returns {Promise<string>}
-     */
-    checkTemplateFlavor(packageJson: any): any;
-
-    /**
-     * @description The method returns details about an app template.
+     * @description The method returns details about a single app template.
      * @param templateName <String>
      * @returns Promise
      */
     getAppTemplateDetails(templateName: string): any;
 
     /**
-     * @description Search for all downloaded templates
+     * @description The method returns details about all app templates.
      * @return Promise
      */
     getTemplates(): any;
-
-    /**
-     * @description Download an app template in your local project folder
-     * @param url <String> - url to the app template repository
-     */
-    downloadAppTemplate(url: string): void;
-
-    /**
-     * @description The method returns details about a page template in JSON Format
-     * @param templateName <String>
-     * @returns Promise
-     */
-    getPageTemplateDetails(templateName: string): any;
-
-    /**
-     * @description Create App method
-     * @param appName <String>
-     * @param location <String>
-     * @returns Promise
-     */
-    createApp(appName: string, location: string): any;
-
-    /**
-     * @description Add page
-     * @param pageName <String>
-     * @returns Promise
-     */
-    addPage(pageName: string, location: string): any;
-
 }
