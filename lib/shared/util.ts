c32ef769e8171dae72434685565190ab9152a661
boris <boris.karastanev@progress.com>
2017-08-23 15:52:38 +0300

    Added a new method getPackageJsonFromSource
    Signed-off-by: boris <boris.karastanev@progress.com>

--- lib/shared/util.ts
+++ lib/shared/util.ts
@@ -2,6 +2,7 @@ import * as nodeUtil from "util";
 
 const indexOf = require("lodash.indexof");
 const sortBy = require("lodash.sortby");
+const request = require("request-promise");
 
 export default class Util {
     static defaultHeaders = {
@@ -11,4 +12,29 @@ export default class Util {
     static indexOf = indexOf;
     static sortBy = sortBy;
     static format = nodeUtil.format;
+    static request = request;
+
+    static getPackageJsonFromSource(url: string) {
+        let content: any;
+
+        return this.request({
+            method: "GET",
+            uri: url,
+            json: true,
+            resolveWithFullResponse: true,
+            headers: this.defaultHeaders
+        })
+            .then((response: any) => {
+                content = response.body;
+                if (content.hasOwnProperty("templateType")) {
+                    return content;
+                }
+            })
+            .catch((error: any) => {
+                return {
+                    message: "Error retrieving package.json from src " + url,
+                    err: error
+                };
+            });
+    }
 }
