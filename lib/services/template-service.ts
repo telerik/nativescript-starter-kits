1830f95e3e78dea722d743076516401c7afdca98
boris <boris.karastanev@progress.com>
2017-09-13 14:42:56 +0300

    Refactored all function and variable abbreviations
    Signed-off-by: boris <boris.karastanev@progress.com>

--- lib/services/template-service.ts
+++ lib/services/template-service.ts
@@ -3,7 +3,7 @@ import util from "../shared/util";
 
 const BACKUP = require("../../consts/templates-backup-data");
 const nodeCache = require("node-cache");
-const tmpCache = new nodeCache();
+const templateCache = new nodeCache();
 
 export class TemplateService implements ITemplateService {
     constructor(private $gitService: IGitService) { }
@@ -82,11 +82,11 @@ export class TemplateService implements ITemplateService {
     }
 
     getTemplates(): Promise<Array<any>> {
-        let tempDetails: Array<any> = [];
+        let templateDetails: Array<any> = [];
         const promises: Array<any> = [];
 
         return new Promise((resolve, reject) => {
-            tmpCache.get("tempDetails", (error: any, value: any) => {
+            templateCache.get("templateDetails", (error: any, value: any) => {
                 if (!error) {
                     if (value === undefined) {
                         this.getTemplatesNames()
@@ -98,7 +98,7 @@ export class TemplateService implements ITemplateService {
                                     promises.push(
                                         this.getAppTemplateDetails(name)
                                             .then((details) => {
-                                                tempDetails.push(details);
+                                                templateDetails.push(details);
                                             })
                                             .catch((errorDetails) => {
                                                 reject(errorDetails);
@@ -108,9 +108,9 @@ export class TemplateService implements ITemplateService {
 
                                 Promise.all(promises)
                                     .then(() => {
-                                        tempDetails = this.sortTmpData(tempDetails);
-                                        tmpCache.set("tempDetails", tempDetails, Config.cacheTime);
-                                        resolve(tempDetails);
+                                        templateDetails = this.sortTemplateData(templateDetails);
+                                        templateCache.set("templateDetails", templateDetails, Config.cacheTime);
+                                        resolve(templateDetails);
 
                                     })
                                     .catch((errorPromises: any) => {
@@ -130,8 +130,8 @@ export class TemplateService implements ITemplateService {
         });
     }
 
-    private sortTmpData(templates: Array<any>) {
-        const flavOrder: Array<string> = ["JavaScript", "TypeScript", "Angular & TypeScript"];
+    private sortTemplateData(templates: Array<any>) {
+        const flavorOrder: Array<string> = ["JavaScript", "TypeScript", "Angular & TypeScript"];
 
         // tslint:disable-next-line:max-line-length
         const typeOrder: Array<string> = [
@@ -142,17 +142,17 @@ export class TemplateService implements ITemplateService {
             "Master-Detail with Kinvey"
         ];
         let sortedByType: Array<any>;
-        let sortByFlav: Array<any>;
+        let sortByFlavor: Array<any>;
 
-        sortedByType = util.sortBy(templates, (temp: any) => {
-            return util.indexOf(typeOrder, temp.displayName);
+        sortedByType = util.sortBy(templates, (template: any) => {
+            return util.indexOf(typeOrder, template.displayName);
         });
 
-        sortByFlav = util.sortBy(sortedByType, (temp: any) => {
-            return util.indexOf(flavOrder, temp.templateFlavor);
+        sortByFlavor = util.sortBy(sortedByType, (temp: any) => {
+            return util.indexOf(flavorOrder, temp.templateFlavor);
         });
 
-        return sortByFlav;
+        return sortByFlavor;
     }
 
     private getTemplatesNames() {
