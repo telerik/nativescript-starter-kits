b4af5aab5100dc9b56a6f648bb5941d7a6d9e662
Alexander Djenkov <djenkov@progress.com>
2017-09-20 13:02:11 +0300

    Modify tests to use chaiAsPromise. Extend template service tests

--- test/test-template-backup.ts
+++ test/test-template-backup.ts
@@ -1,45 +1,54 @@
 import * as chai from "chai";
 import { Config } from "../lib/shared/config";
+
 const expect = chai.expect;
 const templateBackup = require("../consts/templates-backup-data").fallback;
+const chaiAsPromised = require("chai-as-promised");
 
-chai.use(require("chai-things"));
+chai.use(chaiAsPromised);
 
 describe("Template backup data", () => {
 
     describe("Check template backup data integrity", () => {
         it("Should be an Array of Objects", () => {
-            expect(templateBackup).to.be.an("array");
-            expect(templateBackup).to.have.length(Config.availableTemplateRepos.length);
-            templateBackup.should.all.have.property("name");
-            templateBackup.should.all.have.property("description");
-            templateBackup.should.all.have.property("displayName");
-            templateBackup.should.all.have.property("gitUrl");
-            templateBackup.should.all.have.property("version");
-            templateBackup.should.all.have.property("templateFlavor");
-            templateBackup.should.all.have.property("type");
-            templateBackup.should.all.have.property("resources");
+            return expect(Promise.resolve(templateBackup))
+                .to.eventually.be.an("array")
+                .to.have.length(Config.availableTemplateRepos.length).then((array) => {
+                    array.forEach((item: any) => {
+                        expect(item).to.have.property("name");
+                        expect(item).to.have.property("description");
+                        expect(item).to.have.property("displayName");
+                        expect(item).to.have.property("gitUrl");
+                        expect(item).to.have.property("version");
+                        expect(item).to.have.property("templateFlavor");
+                        expect(item).to.have.property("type");
+                        expect(item).to.have.property("resources");
+                    });
+                });
         });
 
         it("Should have platform specific, base64 encoded resource images", () => {
-           for (const template of templateBackup) {
-               expect(template.resources).to.have.property("android");
-               expect(template.resources).to.have.property("ios");
-               expect(template.resources).to.have.property("thumbnail");
-
-               expect(template.resources.android).to.match(/data:image\/png;base64/);
-               expect(template.resources.ios).to.match(/data:image\/png;base64/);
-               expect(template.resources.thumbnail).to.match(/data:image\/png;base64/);
-           }
+            return expect(Promise.resolve(templateBackup))
+                .to.eventually.be.an("array").then((array) => {
+                    array.forEach((item: any) => {
+                        expect(item.resources).to.have.property("android");
+                        expect(item.resources).to.have.property("ios");
+                        expect(item.resources).to.have.property("thumbnail");
+                        expect(item.resources.android).to.match(/data:image\/png;base64/);
+                        expect(item.resources.ios).to.match(/data:image\/png;base64/);
+                        expect(item.resources.thumbnail).to.match(/data:image\/png;base64/);
+                    });
+                });
         });
-    });
 
-    describe("Check template backup data name property", () => {
-        it("names should start with tns-", () => {
-            for (const template of templateBackup) {
-                template.name.should.be.a("string");
-                expect(template.name).to.match(/tns-/);
-            }
+        it("Should have proper name string", () => {
+            return expect(Promise.resolve(templateBackup))
+                .to.eventually.be.an("array").then((array) => {
+                    array.forEach((item: any) => {
+                        expect(item.name).to.match(/tns-/);
+                        expect(item.name).to.be.a("string");
+                    });
+                });
         });
     });
 });
--- test/test-template-service.ts
+++ test/test-template-service.ts
@@ -1,90 +1,215 @@
 import { Yok } from "mobile-cli-lib/yok";
 import { GitService } from "../lib/services/git-service";
 import { TemplateService } from "../lib/services/template-service";
+import { Config } from "../lib/shared/config";
 
+const templateBackup = require("../consts/templates-backup-data").fallback;
 const chai = require("chai");
-const should = require("chai").should();
+const chaiAsPromised = require("chai-as-promised");
+const expect = chai.expect;
+const sinon = require("sinon");
 
-chai.use(require("chai-things"));
+chai.use(chaiAsPromised);
 
 let testInjector: any;
 
 describe("TemplateService Api", () => {
+    let gitService: GitService;
+    let templateService: TemplateService;
+
+    const packageJson = {
+        name: "dummyName",
+        displayName: "dummyDisplayName",
+        version: "1.0",
+        description: "dummyDescription",
+        repository: {
+            url: "dummyUrl"
+        },
+        templateType: "dummyType"
+    };
+
+    const appTemplate = {
+        name: "tns-template-dummy-name",
+        displayName: "dummyDisplayName",
+        version: "1.0",
+        description: "dummyDescription",
+        gitUrl: "dummyUrl",
+        templateType: "dummyType",
+        type: "App template",
+        resources: {
+            android: "android",
+            ios: "ios",
+            thumbnail: "dummyThumbnail"
+        }
+    };
+
     beforeEach(() => {
+        gitService = new GitService();
+        templateService = new TemplateService(gitService);
+
         testInjector = new Yok();
         testInjector.register("templateService", TemplateService);
         testInjector.register("gitService", GitService);
     });
 
     describe("Check template flavor", () => {
-        const gitService = new GitService();
-        const templateService = new TemplateService(gitService);
-        it("Returns a template flavor", () => {
-            templateService.checkTemplateFlavor({name: "template-hello-world-ng"}).then((flavor) => {
-                flavor.should.be.a("string");
-                flavor.should.not.be.an("object");
-                flavor.should.not.be.a("number");
-                flavor.should.not.be.instanceOf(Error);
-            });
+        it("Should be rejected when no packageJson provided", () => {
+            return expect(templateService.checkTemplateFlavor({}))
+                .to.eventually.be.rejectedWith("Cannot read template details")
+                .be.an.instanceOf(Error);
         });
 
-        it("handles errors gracefully ", () => {
-            templateService.checkTemplateFlavor({}).then((flavor) => {
-                should.fail(flavor, null, "flavor should not be returned");
-            }, (errorFlavor) => {
-                errorFlavor.should.be.instanceOf(Error);
-            });
+        it("Should be rejected when no valid packageJson provided", () => {
+            return expect(templateService.checkTemplateFlavor({ noName: "template-hello-world-ng" }))
+                .to.eventually.be.rejectedWith("Cannot read template details")
+                .be.an.instanceOf(Error);
         });
 
+        it("Should return template flavor", () => {
+            return expect(templateService.checkTemplateFlavor({ name: "template-hello-world-ng" }))
+                .to.eventually.be.fulfilled
+                .to.be.equal("Angular & TypeScript");
+        });
     });
 
-    describe("Get App template Details", () => {
-        const gitService = new GitService();
-        const templateService = new TemplateService(gitService);
-        it("Returns a Template Details object via a Promise", () => {
-            templateService.getAppTemplateDetails("template-hello-world-ng").then((details) => {
-                should.exist(details);
-                details.should.be.an("object");
-                details.should.have.property("name");
-                details.should.have.property("description");
-                details.should.have.property("version");
-                details.should.have.property("templateFlavor");
-                details.should.not.be.instanceOf(Error);
-
-            }).catch((err) => {
-                should.not.exist(err);
-            });
+    describe("Get template metadata", () => {
+        it("Should be rejected when no packageJson provided", () => {
+            return expect(templateService.getTemplateMetaData({}))
+                .to.eventually.be.rejectedWith("Missing or invalid package.json provided")
+                .be.an.instanceOf(Error);
         });
 
-        it("It handles error trough Promise Reject", () => {
-            templateService.getAppTemplateDetails("template-hello-world-ng").then((details) => {
-                should.not.exist(details);
-            }).catch((err) => {
-                should.exist(err);
-                err.should.be.instanceOf(Error);
-            });
+        it("Should return template metadata", () => {
+            const metadata = {
+                name: "dummyName",
+                displayName: "dummyDisplayName",
+                version: "1.0",
+                description: "dummyDescription",
+                gitUrl: "dummyUrl",
+                type: "dummyType"
+            };
+
+            return expect(templateService.getTemplateMetaData(packageJson))
+                .to.eventually.be.fulfilled
+                .to.be.an("object")
+                .to.deep.equal(metadata);
+        });
+    });
+
+    describe("Get App template details", () => {
+        let sandbox: any;
+
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create();
+        });
+
+        afterEach(() => sandbox.restore());
+
+        it("Check template details data integrity", () => {
+            const expectedResources = {
+                android: "android",
+                ios: "ios",
+                thumbnail: "dummyThumbnail"
+            };
+
+            sandbox.stub(gitService, "getPackageJsonFromSource")
+                .returns(Promise.resolve(packageJson));
+
+            sandbox.stub(gitService, "getAssetsContent")
+                .returns(Promise.resolve(expectedResources));
+
+            return expect(templateService.getAppTemplateDetails("template-hello-world-ng"))
+                .to.eventually.be.fulfilled
+                .to.be.an("object").then((template: any) => {
+                    expect(template).to.have.property("name", "dummyName");
+                    expect(template).to.have.property("description", "dummyDescription");
+                    expect(template).to.have.property("displayName", "dummyDisplayName");
+                    expect(template).to.have.property("gitUrl", "dummyUrl");
+                    expect(template).to.have.property("version", "1.0");
+                    expect(template).to.have.property("templateFlavor", "JavaScript");
+                    expect(template).to.have.property("type", "dummyType");
+                    expect(template.resources).to.deep.equal(expectedResources);
+                });
+        });
+
+        it("Should be rejected when getPackageJsonFromSource fails", () => {
+            sandbox.stub(gitService, "getPackageJsonFromSource")
+                .returns(Promise.reject(new Error("Error")));
+
+            return expect(templateService.getAppTemplateDetails("template-hello-world-ng"))
+                .to.eventually.be.rejectedWith("Error")
+                .be.an.instanceOf(Error);
+        });
+
+        it("Should be rejected when getAssetsContent fails", () => {
+            sandbox.stub(gitService, "getPackageJsonFromSource")
+                .returns(Promise.resolve(packageJson));
+
+            sandbox.stub(gitService, "getAssetsContent")
+                .returns(Promise.reject(new Error("Error")));
+
+            return expect(templateService.getAppTemplateDetails("template-hello-world-ng"))
+                .to.eventually.be.rejectedWith("Error")
+                .be.an.instanceOf(Error);
         });
     });
 
     describe("Get Available templates", () => {
-        const gitService = new GitService();
-        const templateService = new TemplateService(gitService);
-        it("Returns a Template Details array for all available templates", () => {
-            templateService.getTemplates().then((templates: any) => {
-                should.exist(templates);
-                templates.should.be.an("array");
-                if (templates.length) {
-                    for (let ti = templates.length - 1; ti >= 0; ti--) {
-                        templates[ti].should.have.property("name");
-                        templates[ti].name.should.contain("tns-template-");
-                        templates[ti].should.have.property("version");
-                        templates[ti].should.have.property("description");
-                        templates[ti].should.have.property("templateFlavor");
-                    }
-                }
-            }).catch((err) => {
-                should.not.exist(err);
-            });
+        let sandbox: any;
+
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create();
+        });
+
+        afterEach(() => sandbox.restore());
+
+        it("Should return a Template Details array from cache", () => {
+            templateService.templateCache.set("templateDetails", [appTemplate], 999999);
+
+            return expect(templateService.getTemplates())
+                .to.eventually.be.an("array")
+                .to.have.length(1).then((templates: Array<any>) => {
+                    templates.forEach((template: any) => {
+                        expect(template).to.deep.equal(appTemplate);
+                    });
+                });
+        });
+
+        it("Should return a Template Details array and sets proper cache", () => {
+            const templatesCount = Config.availableTemplateRepos.length;
+            sandbox.stub(templateService, "getAppTemplateDetails")
+                .returns(Promise.resolve(appTemplate));
+
+            return expect(templateService.getTemplates())
+                .to.eventually.be.an("array")
+                .to.have.length(templatesCount).then((templates: Array<any>) => {
+                    templates.forEach((template: any) => {
+                        expect(template).to.deep.equal(appTemplate);
+                    });
+                }).then(() => {
+                    templateService.templateCache.get("templateDetails", (error: any, cachedValue: any) => {
+                        expect(error).to.be.a("null");
+                        expect(cachedValue).to.be.an("array");
+                        expect(cachedValue).to.have.length(templatesCount);
+
+                        cachedValue.forEach((template: any) => {
+                            expect(template).to.deep.equal(appTemplate);
+                        });
+                    });
+                });
+        });
+
+        it("Should return a Template Details array from the backup data", () => {
+            const templatesCount = templateBackup.length;
+
+            sandbox.stub(templateService, "getAppTemplateDetails")
+                .returns(Promise.reject(new Error("Error")));
+
+            return expect(templateService.getTemplates())
+                .to.eventually.be.an("array")
+                .to.have.length(templatesCount).then((templates: Array<any>) => {
+                    expect(templates).to.deep.equal(templateBackup);
+                });
         });
     });
 });
